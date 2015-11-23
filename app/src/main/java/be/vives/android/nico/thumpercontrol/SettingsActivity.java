6e50c40630cf55493653bdf46344e25056a4ea93
Nico De Witte <nico.dewitte@vives.be>
2015-11-23 14:41:15 +0100

    Add string id as setting to app

--- app/src/main/java/be/vives/android/nico/thumpercontrol/SettingsActivity.java
+++ app/src/main/java/be/vives/android/nico/thumpercontrol/SettingsActivity.java
@@ -12,6 +12,7 @@ public class SettingsActivity extends PreferenceActivity {
 
     public static final String KEY_PREF_NODE_IP = "pref_key_node_server_ip";
     public static final String KEY_PREF_NODE_PORT = "pref_key_node_server_port";
+    public static final String KEY_PREF_NEO_STRINGID = "pref_key_neopixel_controller_stringid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
